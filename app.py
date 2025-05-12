2c6c1f96fd4503a5018eaf6f9190f76f87b1bb7f
iaequation <IA@equationcoffee.com>
2025-05-12 16:35:53 -0500

    [UPD] Correr Prueba,py con el api

--- app.py
+++ app.py
@@ -9,8 +9,27 @@ def index():
 @app.route('/run', methods=['POST'])
 def run():
     data = request.get_json()
-    result = {"mensaje": "Hola, recibí tus datos", "datos": data}
-    return jsonify(result)
+    if not data:
+        return jsonify ({"mensaje":"No se recibieron datos"}),400
+    try:
+        result=subprocess.run('python3','prueba.py'],capture_output=True, text=True)
+        if result.return.code==0:
+            return jsonify({
+                "mensaje" : "Datos recibidos y script ejecutado",
+                "datos":data,
+                "script_output":result.stdout
+            })
+        else:
+            return jsonify({
+                "mensaje": "Hubo un error al ejecutar el script",
+                "error": result.stderr
+            }), 500
+    except Exception as e:
+        return jsonify({
+            "mensaje": "Error al ejecutar el script",
+            "error": str(e)
+        }), 500
+        
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
